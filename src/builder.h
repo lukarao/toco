9376e04f1c5a9e561ac8149557b6e9097eac6dbc
Luka <wlukarao@gmail.com>
2024-11-17 19:13:29 -0500

    builder: add BlockRecord, Block, and Record classes

--- src/builder.h
+++ src/builder.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <vector>
+
+class BlockRecord {}; // idk what else to name this
+
+class Block : public BlockRecord {};
+
+class Record : public BlockRecord {};
+
 class Builder {
+    std::vector<BlockRecord> stream;
     public:
         Builder();
 };
